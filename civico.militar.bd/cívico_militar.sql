fbce5cd5494873b298e137dcb1bb1093e63af1aa
Jenifer de Andrade <jeniferandrade2909@gmail.com>
2026-06-10 17:35:44 -0300

    banco de dados -  tabela alunos

--- civico.militar.bd/cívico_militar.sql
+++ civico.militar.bd/cívico_militar.sql
@@ -7,6 +7,20 @@ CREATE TABLE usuarios (
     cpf VARCHAR(14) NOT NULL UNIQUE,
     login VARCHAR(50) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL);
+
+CREATE TABLE alunos (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    matricula VARCHAR(10) NOT NULL UNIQUE,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(14) NOT NULL UNIQUE,	
+    data_nascimento DATE,
+    nome_pai VARCHAR(100),
+    nome_mae VARCHAR(100),
+    endereco VARCHAR(255),
+    curso VARCHAR(50),
+    turno VARCHAR(20),
+    etapa VARCHAR(50),
+    turma VARCHAR(20));
     
 ALTER TABLE alunos
 ADD COLUMN pontuacao_total INTEGER DEFAULT 0;
